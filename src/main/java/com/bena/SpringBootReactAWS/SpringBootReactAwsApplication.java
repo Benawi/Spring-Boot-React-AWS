57f68179ace0fa355e8f803156031685b6430a57
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-02-28 20:55:51 +0300

    spring Boot|React

---
+++ src/main/java/com/bena/SpringBootReactAWS/SpringBootReactAwsApplication.java
@@ -0,0 +1,13 @@
+package com.bena.SpringBootReactAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bena/SpringBootReactAWS/SpringBootReactAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bena.SpringBootReactAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
